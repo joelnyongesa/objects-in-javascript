1def4fc3e727d4248444f6d14c83d593c8509215
joelnyongesa <joelnyongesa148@gmail.com>
2023-07-09 12:11:00 +0300

    Objects in JavaScript

---
+++ index.js
@@ -0,0 +1,15 @@
+// Trying to understand objects in JavaScript.
+
+const wednesdayMenu = {
+  cheesePlate: {
+    soft: "Brie",
+    semiSoft: "Fontina",
+    hard: "Provolone",
+  },
+  fries: "Sweet potato",
+  salad: "Southwestern",
+};
+
+console.log(Object.keys(wednesdayMenu));
+
+console.log(Object.values(wednesdayMenu));

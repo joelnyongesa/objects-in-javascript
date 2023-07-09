6548361ac4926e81a3d121457b98d270546a6d5b
joelnyongesa <joelnyongesa148@gmail.com>
2023-07-09 12:27:52 +0300

    Objects in JavaScript

--- index.js
+++ index.js
@@ -10,6 +10,31 @@ const wednesdayMenu = {
   salad: "Southwestern",
 };
 
-console.log(Object.keys(wednesdayMenu));
+// console.log(Object.keys(wednesdayMenu));
 
-console.log(Object.values(wednesdayMenu));
+// console.log(Object.values(wednesdayMenu));
+
+// MODIFYING OBJECTS.
+const circle = {};
+
+// adding  key using the dot notation.
+circle.radius = 5;
+circle.diameter = 10;
+circle.circumference = ((Math.PI) * circle.diameter);
+circle.area = ((Math.PI)**2)*circle.radius;
+
+// console.log(circle);
+
+// modify a property using the dot notation.
+const mondayMenu = {
+  cheesePlate: {
+    soft: "Chèvre",
+    semiSoft: "Gruyère",
+    hard: "Manchego",
+  },
+  fries: "Curly",
+  salad: "Cobb",
+};
+
+mondayMenu.fries = "Sweet Potato";
+console.log(mondayMenu);
